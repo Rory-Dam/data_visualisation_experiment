02706b3bf2d9c4b165abd2e5c29ad7ec1f41d8e7
Rory Dam <rory@famdam.nl>
2023-06-22 16:07:21 +0200

    moved experiment page out of pages dir and added landing page

--- src/index.py
+++ src/index.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
-st.title('Home page')
+st.set_page_config(layout="wide")
 
+st.title('Welcome')
+st.text('This website showcases two data visualisations developed by Rory Dam for his bachelor thesis at the UvA.')
+st.text('The visualisations show viewership data and have been developed in coöperation with the "Content Intelligence Agency"')
+st.text('Use the side-bar on the left to view and play around with our two visualisations.')
